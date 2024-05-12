9c0c6f4b1b39c7f24a1fc7faebc410f9a2268f48
WillBurbidge <wb734@york.ac.uk>
2024-05-12 21:32:52 +0100

    Added image

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 -- This is where the game will be!
+function love.load()
+	rick = love.graphics.newImage("256.jpg")
+end
 function love.draw()
     love.graphics.print("This is our game, nice :)", 500, 400)
     love.graphics.circle( "fill", 20, 20, 200 )
+    love.graphics.newImage("256.jpg",200,200)
 end
